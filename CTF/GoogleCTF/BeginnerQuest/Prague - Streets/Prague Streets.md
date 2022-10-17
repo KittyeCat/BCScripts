f4d7854168a7625a055f052a37ad75c57918e2db
Basil Connor <basil.connor@hotmail.co.nz>
2022-10-17 21:11:15 +1300

    Prague - Streets SOLVED

--- CTF/GoogleCTF/BeginnerQuest/Prague - Streets/Prague Streets.md
+++ CTF/GoogleCTF/BeginnerQuest/Prague - Streets/Prague Streets.md
@@ -2,3 +2,10 @@ Prague Streets
 
 THis is a coding challenge in JavaScript! we need to write a function named controlCar that takes an input scanArray and outputs driving instructions (-1 = left, 0 = straight, 1 = right)
 
+see Solution.js for details on how this works! you only need to paste the interior of the controlCar function.
+
+flag: CTF{cbe138a2cd7bd97ab726ebd67e3b7126707f3e7f}
+
+we effectively capture the array and split it into three sub arrays, left, ahead and right. we calculate the furthest distance in left and right, and then the closest distance in ahead. 
+
+there is an if statement that compares these three values. If the largest value of one of the side arrays is larger than the smallest value of the ahead array we turn in the direction of the largest side array value. 
--- CTF/GoogleCTF/BeginnerQuest/Prague - Streets/Solution.js
+++ CTF/GoogleCTF/BeginnerQuest/Prague - Streets/Solution.js
@@ -2,7 +2,7 @@ let a = new Array(0, 0, 0, 50, 50, 50, 300, 20, 20, 20, 300, 30, 30, 30, 0, 0, 0
 
 function controlCar(scanArray) {
     for (let i = 0; i <= scanArray.length; i++) {
-        if (scanArray[i] > 100){
+        if (scanArray[i] > 1000){
             scanArray[i] = 0;
 //            console.log(i)
         }
@@ -11,28 +11,44 @@ function controlCar(scanArray) {
         }
     }
 
-    let left = scanArray.slice(0, 8);
-    let right = scanArray.slice(9, scanArray.length);
-    let ahead = scanArray[8];
+    let left = scanArray.slice(0, 7);
+    let right = scanArray.slice(10, scanArray.length);
+    let ahead = scanArray.slice(7, 10);
+    let aheadMin = ahead.sort(function(a, b){return b - a})[2];
 
     left.forEach((element, index) => {
-        if (element <= ahead) {
+        if (element <= aheadMin) {
             left[index] = 0;
         }
     });
     left.sort(function(a, b){return b - a});
 
     right.forEach((element, index) => {
-        if (element <= ahead) {
+        if (element <= aheadMin) {
             right[index] = 0;
         }
     });
     right.sort(function(a, b){return b - a});
 
     console.log(ahead);
+    console.log(aheadMin);
     console.log(scanArray);
     console.log(left);
     console.log(right);
+
+
+    if (left[0] > right[0] && left[0] > aheadMin) {
+        console.log(-1);
+        return -1;
+    }
+    else if (left[0] < right[0] && right[0] > aheadMin) {
+        console.log(1);
+        return 1;
+    }
+    else {
+        console.log(0);
+        return 0;
+    }
 
 
 }
